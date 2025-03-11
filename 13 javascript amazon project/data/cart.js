31de41e390190d3c7765f13a7e171abc52f4fea6
Natsuyoshi <dara.tamako@gmail.com>
2025-03-11 15:09:03 +0100

    Move addToCart into cart.js

--- 13 javascript amazon project/data/cart.js
+++ 13 javascript amazon project/data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {    //if item is in cart, increase
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({  //adds product in cart
+      productId: productId,
+      quantity: 1
+    });
+  }
+}
